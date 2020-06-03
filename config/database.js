ce7fe25b1ba59072cb41112a5fd8d9ceb757f9e2
Rakesh Khot <rakeshkhot18@gmail.com>
2020-06-03 17:42:43 +0530

    added

---
+++ config/database.js
@@ -0,0 +1,11 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': '127.0.0.1', //
+        'user': 'root', //
+        'password': 'root', //
+        'database': 'test'//
+    },
+	'database': 'test',
+
+};
